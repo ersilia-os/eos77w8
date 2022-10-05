84ec2a141fa203cc3856d9cbedaa5c20b77a5a1f
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2022-10-05 08:52:58 +0200

    Update main.py

--- model/framework/grover/main.py
+++ model/framework/grover/main.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/sider', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/sider/fold_0/model_0/model.pt', model_path+'/framework/finetune/sider/fold_2/model_0/model.pt', model_path+'/framework/finetune/sider/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=[features_path], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
 
 
-    sf.save_features_main(csv_path)
+    sf.save_features_main(csv_path, features_path)
 
     train_args = get_newest_train_args()
     avg_preds, test_smiles = make_predictions(args, train_args)
